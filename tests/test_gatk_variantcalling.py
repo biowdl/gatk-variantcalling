bb1252a349f0031ae5bb894319d366209b478c9b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-01-21 07:54:34 +0100

    add tests

--- tests/test_gatk_variantcalling.py
+++ tests/test_gatk_variantcalling.py
@@ -18,20 +18,37 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import os
+import subprocess
+import tempfile
 from pathlib import Path
-from cyvcf2 import Variant, VCF
 
 import pytest
 
+TEST_DATA = Path(Path(__file__).parent, "data")
+
+
+def create_sdf(sequence_path: Path) -> Path:
+    # We need a not yet existing directory. Hence mkstemp. (Even though this
+    # is unsafe as another tempfile process may claim the directory in theory.)
+    output_sdf = tempfile.mkstemp()
+    args = ["rtg", "format", "-o", output_sdf, str(sequence_path)]
+    subprocess.run(args, check=True)
+    return Path(output_sdf)
+
+
 @pytest.mark.workflow("two_sample_gender_aware")
-def test_variants_gender_aware(workflow: Path):
-    vcf_file = Path(workflow, "test-output", "multisample.vcf.gz")
-    vcf = VCF(str(vcf_file))
-    try:
-        samples = vcf.samples
-        for variant in vcf:  # type: Variant
-            variant.genotype
-
-    finally:
-        vcf.close()
-
+def test_variants_gender_aware(workflow_dir: Path):
+    baseline_vcf_file = Path(TEST_DATA, "gender-aware", "expected.vcf.gz")
+    calls_vcf_file = Path(workflow_dir, "test-output", "multisample.vcf.gz")
+    reference_fasta = Path(TEST_DATA, "gender-aware", "reference.fasta")
+    sdf_path = create_sdf(reference_fasta)
+    output_dir = Path(tempfile.mkdtemp())
+    args = ["rtg", "vcfeval",
+            "-b", str(baseline_vcf_file),
+            "-c", str(calls_vcf_file),
+            "-t", str(sdf_path),
+            "-o", str(output_dir),
+            "--sample", "male"]
+    subprocess.run(args, check=True)
+    print(output_dir)
